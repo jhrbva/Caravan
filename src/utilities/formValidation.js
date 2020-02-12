c59c7a79d5ce2cad268e555e14f1b09f4fd11836
Khristian Brooks <kh.brooks02@gmail.com>
2020-02-12 17:03:05 -0500

    add functions for form validation

---
+++ src/utilities/formValidation.js
@@ -0,0 +1,8 @@
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phoneRegex = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+
+export const validateEmail = email =>
+	emailRegex.test(String(email).toLowerCase());
+
+export const validatePhone = number =>
+	phoneRegex.test(String(number).toLowerCase());
