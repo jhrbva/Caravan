8e458cd3604ef83ba93985246f2796c85081857b
Chantelle Levy <chanie01234@users.noreply.github.com>
2020-02-13 16:02:18 -0500

    added structure to files and fixed server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const express = require('express'); // grab express module installed
+const app = express(); // created an app using express module
+const port = 8080;
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+app.get('/', (req, res) => res.send('Hello World!')); // express is handling a GET request
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
