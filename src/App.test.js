f7e1b2f1c59a6355cf980a78570a0256ec52f471
connie-code <conniewu06@gmail.com>
2020-02-20 00:40:33 -0500

    Cleaned up commented code

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,10 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
-//
-// xtest('renders learn react link', () => {
-// 	const { getByText } = render(<App />);
-// 	const linkElement = getByText(/learn react/i);
-// 	expect(linkElement).toBeInTheDocument();
-// });
-
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-// it('renders without crashing', () => {
-//   shallow(<App />);
-// });
 
 it('renders welcome message', () => {
   const wrapper = shallow(<App />);
   const welcome = <h2>Welcome to React</h2>;
-  // expect(wrapper.contains(welcome)).toEqual(true);
 	expect(wrapper).toContainReact(welcome);
 });
