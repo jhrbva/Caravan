aaf20798803d6f8c8ccfdca97cde15cc7735c4d5
Chantelle Levy <chanie01234@users.noreply.github.com>
2020-02-26 21:00:06 -0500

    Revert "added some elements to make a HTML page"
    This reverts commit 3e2b225007b781e21a0f0796aa7fd00e79baf378.

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
 const express = require('express'); // grab express module installed
 const app = express(); // created an app using express module
 const port = 8080;
-var passport = require('passport');
 
-// main menu route
-app.get('/', function (req, res) {
-  var html = "<ul>\
-    <li><a href='/auth/github'>GitHub</a></li>\
-    <li><a href='/logout'>logout</a></li>\
-  </ul>";
+app.get('/', (req, res) => res.send('Hello World!')); // express is handling a GET request
 
-  res.send(html);
-});
-
-var server = app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`)});
-
-app.get('/logout', function(req, res){
-  console.log('logging out');
-  req.logout();
-  res.redirect('/');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
