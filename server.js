9e46eab3216037ce95fec290dc9ba53595e6736d
Chantelle Levy <chanie01234@users.noreply.github.com>
2020-02-28 13:55:53 -0500

    added passportjs packages and setup a HTML page for future testing of google and fb auth

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express'); // grab express module installed
 const app = express(); // created an app using express module
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!')); // express is handling a GET request
+app.get('/', function (req, res) {
+  var html = " <ul>\
+    <li><a href='/auth/facebook'>Facebook</a></li>\
+    <li><a href='/logout'>Logout</a></li>\
+    <li><a href='/auth/google'>Google</a></li>\
+    <li><a href='/logout'>Logout</a></li>\
+  </ul>";
+
+  res.send(html);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+var server = app.listen(port, function () {console.log('Example app listening on port ${port}!')});
