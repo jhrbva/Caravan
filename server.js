5b013219d5c5aa3fcf58df1709a2b0c5b00409c4
Chantelle Levy <chanie01234@users.noreply.github.com>
2020-02-28 14:02:14 -0500

    fixed HTML page on server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express(); // created an app using express module
 const port = 8080;
 
 app.get('/', function (req, res) {
-  var html = " <ul>\
+  var html = "<ul>\
     <li><a href='/auth/facebook'>Facebook</a></li>\
     <li><a href='/logout'>Logout</a></li>\
     <li><a href='/auth/google'>Google</a></li>\
@@ -13,5 +13,4 @@ app.get('/', function (req, res) {
   res.send(html);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 var server = app.listen(port, function () {console.log('Example app listening on port ${port}!')});
