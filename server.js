de2629d4a4a9a61072774c3daafa352fc2440c35
Julia Helena <jh@juliahelena.me>
2020-02-29 14:58:27 -0500

    left a comment on server.js to indicate where to pick up the work from

--- server.js
+++ server.js
@@ -10,12 +10,18 @@ const pool = new Pool({
 	password: 'tazoicedblacktea',
 	port: 5432,
 });
+// continue here: instead of GET we need POST request
+// app.get('/login', (req, res) => {
+//     req = req.user , req.password
+//     pool.query('SELECT * FROM userTable WHERE username='req.user' AND password='req.password';', (err, response) => {
+//         console.log(err, response.rows);
+//         pool.end();
+//     });
+// })
 
-pool.query('SELECT * FROM userTable LIMIT 1;', (err, res) => {
-	console.log(err, res);
-	pool.end();
-});
-
-app.get('/', (req, res) => res.send('Hello World!')); // express is handling a GET request
+app.get('/', (req, res) => {
+	console.log(req.body);
+	return res.send('Hello World!');
+}); // express is handling a GET request
 
 app.listen(port, () => console.log(`Example app running on port ${port}!`));
