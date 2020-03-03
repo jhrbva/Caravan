cca89fd1f511150c214d437ccbe51e9a1cc7e91a
Julia Helena <jh@juliahelena.me>
2020-03-02 21:50:36 -0500

    fixing trip guests tests

--- src/components/TripForm/TripGuests.test.jsx
+++ src/components/TripForm/TripGuests.test.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import TripGuests from './TripForm';
+import TripGuests from './TripGuests';
 
 describe('TripGuests', () => {
-    it('should match the snapshot', () => {
-        const wrapper = shallow(<TripGuests />);
-        expect(wrapper).toMatchSnapshot();
-    });
-    it('should have one field', () => {
-        const wrapper = shallow(<TripGuests />);
-        const fieldWrapper = wrapper.find('Field');
-        expect(fieldWrapper.length).toBe(1);
-    });
-    it('should have an guests field', () => {
-        const wrapper = shallow(<TripGuests />);
-        const fieldWrapper = wrapper.find('Field');
-        const usernameField = fieldWrapper.find({ name: 'guests' });
-        expect(usernameField.length).toBe(1);
-    });
+	it('should match the snapshot', () => {
+		const wrapper = shallow(<TripGuests />);
+		expect(wrapper).toMatchSnapshot();
+	});
+	it('should have one field', () => {
+		const wrapper = shallow(<TripGuests />);
+		const fieldWrapper = wrapper.find('Field');
+		expect(fieldWrapper.length).toBe(1);
+	});
+	it('should have an guests field', () => {
+		const wrapper = shallow(<TripGuests />);
+		const fieldWrapper = wrapper.find('Field');
+		const usernameField = fieldWrapper.find({ name: 'guests' });
+		expect(usernameField.length).toBe(1);
+	});
 });
