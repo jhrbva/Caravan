dfd6c25cac4aea4f118fb738c4f57a3e3f93a9bf
Julia Helena <jh@juliahelena.me>
2020-03-03 16:59:37 -0500

    adding tests to trip form states

--- src/components/TripForm/TripForm.test.jsx
+++ src/components/TripForm/TripForm.test.jsx
@@ -7,4 +7,14 @@ describe('TripForm', () => {
 		const wrapper = shallow(<TripForm />);
 		expect(wrapper).toMatchSnapshot();
 	});
+	it('changes page to Trip Guests', () => {
+		const wrapper = shallow(<TripForm />);
+		wrapper.setState({ page: 2 });
+		expect(wrapper.state('page')).toEqual(2);
+	});
+	it('changes page to Trip Rest Stops', () => {
+		const wrapper = shallow(<TripForm />);
+		wrapper.setState({ page: 3 });
+		expect(wrapper.state('page')).toEqual(3);
+	});
 });
