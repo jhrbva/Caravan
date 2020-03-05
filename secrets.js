1a4ea7f1b0bc01f019ce80331f75cda266c4e361
Khristian Brooks <kh.brooks02@gmail.com>
2020-03-05 10:19:20 -0500

    remove secrets

--- secrets.js
+++
@@ -1,7 +0,0 @@
-// Temporary Solution
-const DB_USER = 'postgres';
-const DB_HOST = 'database-4.ckeaom6xxsow.us-east-2.rds.amazonaws.com';
-const DB_NAME = 'Pepperoni';
-const DB_PASSWORD = 'tazoicedblacktea';
-
-module.exports = { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD };
