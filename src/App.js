a2e25a1800dde536f5b457c23d95de5840db9ae7
quetourah <dina.qd.academics@gmail.com>
2020-03-05 23:30:53 -0500

    add logos to App
    - add the favicon for the browser tab
    - add the app's logo to app

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TripForm from './components/TripForm/TripForm';
+import caravan from './caravan.png';
 
 function App() {
 	return (
 		<Router>
 			<div className='App'>
 				<header className='App-header'>
-					<h2>Welcome to React</h2>
-					<h1 className='temp-logo'>Caravan</h1>
+					<h2>Welcome to the Caravan App</h2>
+					<img src={caravan} className="App-logo" alt="Caravan" />
+					{/*<h1 className='temp-logo'>Caravan</h1>*}
 					{/* TODO: CA-30 create logo */}
 
 					<Route exact path='/'>
