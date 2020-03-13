5b3b401a150a590712dcd86d265b62c814ff8dcb
jhrbva <jrobazzi@gmail.com>
2020-03-13 17:20:18 -0400

    deleting styles for now

--- src/components/TripDetails/TripDetails.jsx
+++ src/components/TripDetails/TripDetails.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 
 import './TripDetails.scss';
 import Navbar from '../Navbar/Navbar';
-import illustration from '../../assets/trip-details-art.png';
 
 export const TripDetails = props => {
 	return (
 		<div>
 			<Navbar />
 			<div className='trip-details-wrapper'>
-				<img
-					src={illustration}
-					className='trip-details-illustration'
-					alt='car on the road'
-				/>
 				<div className='trip-details'>
 					<h1 className='header-text'>Name of the trip</h1>
 					<h3 className='header-text-light'>Created by @host</h3>
