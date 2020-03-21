87af1c7e33b29a4ca8635b3a03fd8ce736457db5
Khristian Brooks <kh.brooks02@gmail.com>
2020-03-21 14:15:55 -0400

    use original names

--- src/components/TripForm/TripForm.jsx
+++ src/components/TripForm/TripForm.jsx
@@ -17,25 +17,18 @@ class TripContainer extends React.Component {
 		};
 	}
 
-	next = values =>
+	nextPage = values =>
 		this.setState(state => ({
 			page: Math.min(state.page + 1, this.props.children.length - 1),
 			values,
 		}));
 
-	previous = () =>
+	previousPage = () =>
 		this.setState(state => ({
 			page: Math.max(state.page - 1, 0),
 		}));
 
-	validate = values => {
-		const activePage = React.Children.toArray(this.props.children)[
-			this.state.page
-		];
-		return activePage.props.validate ? activePage.props.validate(values) : {};
-	};
-
-	handleSubmit = (values, bag) => {
+	onSubmit = (values, bag) => {
 		const { children, onSubmit } = this.props;
 		const { page } = this.state;
 		const isLastPage = page === React.Children.count(children) - 1;
@@ -44,7 +37,7 @@ class TripContainer extends React.Component {
 		} else {
 			bag.setTouched({});
 			bag.setSubmitting(false);
-			this.next(values);
+			this.nextPage(values);
 		}
 	};
 
@@ -57,7 +50,6 @@ class TripContainer extends React.Component {
 			<Formik
 				initialValues={values}
 				enableReinitialize={false}
-				validate={this.validate}
 				onSubmit={this.handleSubmit}
 				render={({ values, handleSubmit, isSubmitting, handleReset }) => (
 					<form onSubmit={handleSubmit}>
@@ -66,7 +58,7 @@ class TripContainer extends React.Component {
 							{page > 0 && (
 								<BigButton
 									type='button'
-									onClick={this.previous}
+									onClick={this.previousPage}
 									value='Previous'
 								/>
 							)}
