08128a87ae36b697934a2f556feaf5cd1439d435
Khristian Brooks <kh.brooks02@gmail.com>
2020-03-25 11:13:18 -0400

    add button from dashboard to trip form

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Dashboard() {
-	return <>Dashboard</>;
+	return (
+		<>
+			<h1>Dashboard</h1>
+			<Link to='/trip'>
+				<Button variant='success'>New Trip +</Button>
+			</Link>
+		</>
+	);
 }
 
 export default Dashboard;
