1aa86d8a5c277ab49ef0405f6b118f1756b9b973
Khristian Brooks <kh.brooks02@gmail.com>
2020-04-04 20:53:40 -0400

    add your trips and trips joined section

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import SummaryCard from '../SummaryCard/SummaryCard';
 
@@ -8,22 +8,50 @@ export default class Dashboard extends React.Component {
 		super(props);
 		this.state = {
 			invitations: [],
+			tripsJoined: [],
+			tripsHosted: [],
 		};
 	}
 
 	componentDidMount() {
 		// TO DO: add redux to dynamically import user id
 		fetch('/invitations/3')
-			.then(response => {
+			.then((response) => {
 				return response.json();
 			})
-			.then(data => {
+			.then((data) => {
 				this.setState({ invitations: data });
+			});
+		fetch('/trips/2')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log('trip data: ', data);
+				this.setState({ tripsJoined: data.tripsJoined });
+				this.setState({ tripsHosted: data.tripsHosted });
 			});
 	}
 
+	renderSection = (title, data) => {
+		return (
+			<>
+				<h3>{title}</h3>
+				<Row>
+					{data.map((entry, id) => {
+						return (
+							<Col>
+								<SummaryCard key={id} trip={entry} />
+							</Col>
+						);
+					})}
+				</Row>
+			</>
+		);
+	};
+
 	render() {
-		const { invitations } = this.state;
+		const { invitations, tripsJoined, tripsHosted } = this.state;
 
 		return (
 			<>
@@ -31,10 +59,9 @@ export default class Dashboard extends React.Component {
 				<Link to='/trip'>
 					<Button variant='success'>New Trip +</Button>
 				</Link>
-				<h3> Invitations </h3>
-				{invitations.map((invite, id) => {
-					return <SummaryCard key={id} trip={invite} />;
-				})}
+				{this.renderSection('Invitations', invitations)}
+				{this.renderSection('Your Trips', tripsHosted)}
+				{this.renderSection('Trips Joined', tripsJoined)}
 			</>
 		);
 	}
