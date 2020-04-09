c043ffe9f1cd93552cc840168871333b9b37c535
jhrbva <jrobazzi@gmail.com>
2020-04-09 16:46:28 -0500

    suggest edit component frame

---
+++ src/components/SuggestEdit/SuggestEdit.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import BigButton from '../BigButton/BigButton';
+
+const SuggestEdit = () => {};
+
+export default SuggestEdit;
