9d35df37420f793d4adf4cdd19ea0a5335c47bcc
jhrbva <jrobazzi@gmail.com>
2020-04-11 11:39:17 -0500

    starting function for post request

--- src/components/SuggestEdit/SuggestEdit.jsx
+++ src/components/SuggestEdit/SuggestEdit.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+
 import { Field, Form, Formik } from 'formik';
 import '../../utilities/fonts.scss';
 import { Link } from 'react-router-dom';
 import BigButton from '../BigButton/BigButton';
+
+const submitSuggestion = (response) => {
+	// const { userid, tripid, responseCode } = response;
+	// axios
+	// 	.post('/itineraryrequest', {
+	// 		userid: userid,
+	// 		tripid: tripid,
+	// 		accepted: true,
+	// 	})
+	// 	.then(function (response) {
+	// 		console.log(response);
+	// 	})
+	// 	.catch(function (error) {
+	// 		console.log(error);
+	// 	});
+};
 
 const SuggestEdit = () => {
 	return (
@@ -14,7 +32,15 @@ const SuggestEdit = () => {
 					component='textarea'
 				/>
 				<Link to='/dashboard'>
-					<BigButton value={'Send'} />
+					<BigButton
+						value={'Send'}
+						// onClick={() => {
+						// 	submitSuggestion({
+						// 		tripid,
+						// 		value,
+						// 	});
+						// }}
+					/>
 				</Link>
 			</Form>
 		</Formik>
