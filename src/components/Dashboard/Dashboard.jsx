44e0d6229784183b1347ac97ee347bb27f6ba530
jhrbva <jrobazzi@gmail.com>
2020-04-17 17:44:32 -0500

    merging master

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import SummaryCard from '../SummaryCard/SummaryCard';
 
@@ -8,6 +8,8 @@ export default class Dashboard extends React.Component {
 		super(props);
 		this.state = {
 			invitations: [],
+			tripsJoined: [],
+			tripsHosted: [],
 			host: [],
 			members: [],
 			reststops: [],
@@ -18,46 +20,91 @@ export default class Dashboard extends React.Component {
 		// TO DO: add redux to dynamically import user id
 		Promise.all([
 			fetch('/invitations/3'),
+			fetch('/trips/2'),
 			fetch('/members/1'),
 			fetch('/reststop/1'),
 		])
-			.then(([response1, response2, response3]) => {
+			.then(([response1, response2, response3, response4]) => {
 				return Promise.all([
 					response1.json(),
 					response2.json(),
 					response3.json(),
+					response4.json(),
 				]);
 			})
-			.then(([response1, response2, response3]) => {
+			.then(([response1, response2, response3, response4]) => {
 				this.setState({ invitations: response1 });
-				this.setState({ host: response2.host[0].username });
-				this.setState({ members: response2.members });
-				this.setState({ reststops: response3 });
+				this.setState({
+					tripsJoined: response2.tripsJoined,
+					tripsHosted: response2.tripsHosted,
+				});
+				this.setState({ host: response3.host[0].username });
+				this.setState({ members: response3.members });
+				this.setState({ reststops: response4 });
 			});
 	}
 
+	renderSection = (title, type, host, members, reststops) => {
+		// console.log(title, data);
+		const nullResponse =
+			title === 'Invitations' ? 'No invitations' : 'No trips';
+		return (
+			<>
+				<h3>{title}</h3>
+				<Row>
+					{type.length ? (
+						type.map((entry, id) => {
+							return (
+								<Col>
+									<SummaryCard
+										key={id}
+										trip={entry}
+										host={host}
+										members={members}
+										reststops={reststops}
+									/>
+								</Col>
+							);
+						})
+					) : (
+						<p>{nullResponse}</p>
+					)}
+				</Row>
+			</>
+		);
+	};
+
 	render() {
-		const { invitations, host, members, reststops } = this.state;
+		const {
+			invitations,
+			tripsJoined,
+			tripsHosted,
+			host,
+			member,
+			reststops,
+		} = this.state;
+
 		return (
 			<>
 				<h1>Dashboard</h1>
 				<Link to='/trip'>
 					<Button variant='success'>New Trip +</Button>
 				</Link>
-				<h3> Invitations </h3>
-				{invitations.map((invite, key) => {
-					return (
-						<>
-							<SummaryCard
-								key={key}
-								trip={invite}
-								host={host}
-								members={members}
-								reststops={reststops}
-							/>
-						</>
-					);
-				})}
+				{this.renderSection(
+					'Invitations',
+					invitations,
+					host,
+					member,
+					reststops
+				)}
+				{this.renderSection('Your Trips', tripsHosted, host, member, reststops)}
+				{this.renderSection(
+					'Trips Joined',
+					tripsJoined,
+					host,
+					member,
+					reststops
+				)}
 			</>
 		);
 	}
