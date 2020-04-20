389475cd8c769a871a155c5b9f9ef1fc8868f2df
quetourah <dina.qd.academics@gmail.com>
2020-04-20 17:01:58 -0400

    update input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { InputGroup, Form, FormControl } from 'react-bootstrap';
+import React from "react";
 
-import './Input.scss';
+import "./Input.scss";
+import TextField from "@material-ui/core/TextField";
 
 function Input({
 	type,
@@ -19,27 +19,22 @@ function Input({
 		form && field && form.errors[field.name] && form.touched[field.name];
 	return (
 		<div className='input-wrapper'>
-			<InputGroup className='mb-3'>
-				<InputGroup.Prepend>
-					<InputGroup.Text>{icon}</InputGroup.Text>
-				</InputGroup.Prepend>
-				<FormControl
-					type={type}
-					name={name}
-					isInvalid={form && !!form.errors[field.name]}
-					placeholder={placeholder}
-					maxLength={maxLength}
-					autoFocus={autofocus}
-					required
-					{...field}
-					{...props}
-				/>
-				{errorExists && (
-					<Form.Control.Feedback className='error-text' type='invalid'>
-						{form.errors[field.name]}
-					</Form.Control.Feedback>
-				)}
-			</InputGroup>
+			<i className='material-icons'>{icon}</i>
+			<input
+				className='input-outlined'
+				label='Required'
+				type={type}
+				name={name}
+				isInvalid={form && !!form.errors[field.name]}
+				placeholder={placeholder}
+				maxLength={maxLength}
+				autoFocus={autofocus}
+				required
+				{...field}
+				{...props}
+			/>
+
+			{/*errorExists && form.errors[field.name]*/}
 		</div>
 	);
 }
