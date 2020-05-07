bb27524a7820eaab75d932582a0c39f21d399f60
connie-code <conniewu06@gmail.com>
2020-05-07 17:36:36 -0400

    Updated trips and invitations endpoint so that member's id is extracted

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ async function getInvitations(userid){
 
 	async function addMembers(trips) {
 		const res = await	pool.query(
-			'SELECT usertable.username FROM members JOIN usertable on (members.userid = usertable.userid) WHERE tripid=$1',
+			'SELECT usertable.username, members.userid FROM members JOIN usertable on (members.userid = usertable.userid) WHERE tripid=$1',
 			[trips.tripid]
 		);
 		return { ...trips, members: res.rows };
@@ -360,7 +360,7 @@ async function getTrips(userid){
 
 	async function addMembers(trips) {
 		const res = await	pool.query(
-			'SELECT usertable.username FROM members JOIN usertable on (members.userid = usertable.userid) WHERE tripid=$1',
+			'SELECT usertable.username, members.userid FROM members JOIN usertable on (members.userid = usertable.userid) WHERE tripid=$1',
 			[trips.tripid]
 		);
 		return { ...trips, members: res.rows };
