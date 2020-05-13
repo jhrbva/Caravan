ca7f4baafbb2b52520dd37c29cde2522b3a15e3d
jhrbva <jrobazzi@gmail.com>
2020-05-12 19:10:13 -0500

    cleaning up styles for turn-by-turn navigation

--- src/components/Map/InstructionalArrows.jsx
+++ src/components/Map/InstructionalArrows.jsx
@@ -2,7 +2,6 @@
 // instructions and passes them to the instructional overlay
 
 import React from 'react';
-import NavigationIcon from '@material-ui/icons/Navigation';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import CallMadeIcon from '@material-ui/icons/CallMade';
@@ -15,47 +14,48 @@ import DirectionsBoatIcon from '@material-ui/icons/DirectionsBoat';
 import TrainIcon from '@material-ui/icons/Train';
 import MergeTypeIcon from '@material-ui/icons/MergeType';
 import CallSplitIcon from '@material-ui/icons/CallSplit';
+import './InstructionalOverlay.scss';
 
 const InstructionalArrows = (maneuver) => {
 	switch (maneuver.maneuver) {
 		case 'straight':
-			return <ArrowUpwardIcon style={{ fontSize: 80 }} />;
+			return <ArrowUpwardIcon className={'maneuver'} />;
 		case 'merge':
-			return <MergeTypeIcon style={{ fontSize: 80 }} />;
+			return <MergeTypeIcon className={'maneuver'} />;
 		case 'ferry':
-			return <DirectionsBoatIcon style={{ fontSize: 80 }} />;
+			return <DirectionsBoatIcon className={'maneuver'} />;
 		case 'ferry-train':
-			return <TrainIcon style={{ fontSize: 80 }} />;
+			return <TrainIcon className={'maneuver'} />;
 		case 'turn-sharp-left':
-			return <ArrowBackIcon style={{ fontSize: 80 }} />;
+			return <ArrowBackIcon className={'maneuver'} />;
 		case 'turn-slight-left':
-			return <ArrowBackIcon style={{ fontSize: 80 }} />;
+			return <ArrowBackIcon className={`maneuver top-left`} />;
 		case 'ramp-left':
-			return <ArrowBackIcon style={{ fontSize: 80 }} />;
+			return <ArrowBackIcon className={`maneuver top-left`} />;
 		case 'turn-left':
-			return <ArrowBackIcon style={{ fontSize: 80 }} />;
+			return <ArrowBackIcon className={'maneuver'} />;
 		case 'uturn-left':
-			return <UndoIcon style={{ fontSize: 80 }} />;
+			return <UndoIcon className={'maneuver'} />;
 		case 'roundabout-left':
-			return <RotateLeftIcon style={{ fontSize: 80 }} />;
+			return <RotateLeftIcon className={'maneuver'} />;
 		case 'fork-left':
-			return <CallSplitIcon style={{ fontSize: 80 }} />;
+			return <CallSplitIcon className={'maneuver'} />;
 		case 'turn-sharp-right':
-			return <ArrowForwardIcon style={{ fontSize: 80 }} />;
+			return <ArrowForwardIcon className={'maneuver'} />;
 		case 'turn-slight-right':
-			return <CallMadeIcon style={{ fontSize: 80 }} />;
+			return <CallMadeIcon className={'maneuver'} />;
 		case 'ramp-right':
-			return <CallMadeIcon style={{ fontSize: 80 }} />;
+			return <CallMadeIcon className={'maneuver'} />;
 		case 'turn-right':
-			return <ArrowForwardIcon style={{ fontSize: 80 }} />;
+			return <ArrowForwardIcon className={'maneuver'} />;
 		case 'uturn-right':
-			return <RedoIcon style={{ fontSize: 80 }} />;
+			return <RedoIcon className={'maneuver'} />;
 		case 'roundabout-right':
-			return <RotateRightIcon style={{ fontSize: 80 }} />;
+			return <RotateRightIcon className={'maneuver'} />;
 		case 'fork-right':
-			return <CallSplitIcon style={{ fontSize: 80 }} />;
+			return <CallSplitIcon className={'maneuver'} />;
 		default:
-			return <NavigationIcon style={{ fontSize: 80 }} />;
+			return '';
 	}
 };
 
--- src/components/Map/InstructionalOverlay.jsx
+++ src/components/Map/InstructionalOverlay.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import InstructionalArrows from './InstructionalArrows';
-import { Row, Col } from 'react-bootstrap';
 import './InstructionalOverlay.scss';
 
 export default class InstructionalOverlay extends React.Component {
@@ -26,48 +25,22 @@ export default class InstructionalOverlay extends React.Component {
 	};
 
 	componentDidMount = () => {
-		const testDirections = {
-			instruction: [
-				[40.7, -74.0, 'turn-sharp-left', 'turn-sharp-left'],
-				[40.7, -74.0, 'turn-slight-left', 'turn-slight-left'],
-				[40.7, -74.0, 'uturn-left', 'uturn-left'],
-				[40.7, -74.0, 'turn-left', 'turn-left'],
-				[40.7, -74.0, 'roundabout-left', 'roundabout-left'],
-				[40.7, -74.0, 'fork-left', 'fork-left'],
-				[40.7, -74.0, 'ramp-left', 'ramp-left'],
-				[40.7, -74.0, 'uturn-right', 'uturn-right'],
-				[40.7, -74.0, 'turn-slight-right', 'turn-slight-right'],
-				[40.7, -74.0, 'turn-right', 'turn-right'],
-				[40.7, -74.0, 'roundabout-right', 'roundabout-right'],
-				[40.7, -74.0, 'ramp-right', 'ramp-right'],
-				[40.7, -74.0, 'fork-right', 'fork-right'],
-				[40.7, -74.0, 'turn-sharp-right', 'turn-sharp-right'],
-				[40.7, -74.0, 'merge', 'merge'],
-				[40.7, -74.0, 'straight', 'straight'],
-				[40.7, -74.0, 'ferry-train', 'ferry-train'],
-				[40.7, -74.0, 'ferry', 'ferry'],
-			],
-		};
-		this.getNextStep(testDirections);
-		// this.getNextStep(this.props);
+		this.getNextStep(this.props);
 	};
 
 	render() {
 		return (
-			<Row style={{ backgroundColor: '#056638', color: 'white' }}>
-				<Col md={2}>
+			<div className='instructional-overlay'>
+				<div className='arrow-wrapper'>
 					<InstructionalArrows maneuver={this.state.maneuver} />
-				</Col>
-				<Col md={8}>
-					<h4 className='align-middle'>
-						<div
-							dangerouslySetInnerHTML={{
-								__html: this.state.currentInstructionMessage,
-							}}
-						/>
-					</h4>
-				</Col>
-			</Row>
+				</div>
+				<div
+					className='instruction-wrapper'
+					dangerouslySetInnerHTML={{
+						__html: this.state.currentInstructionMessage,
+					}}
+				/>
+			</div>
 		);
 	}
 }
